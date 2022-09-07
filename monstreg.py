c8c764e29a802d51cf6a6d99a43f39761a32eb11
hinata810 <hinata810.hinatab0507@gmail.com>
2022-09-07 09:41:10 +0200

    resolution du conflit

---
+++ monstreg.py
@@ -0,0 +1,14 @@
+import random
+
+def genMob(nom):
+    PV = random.randint(5, 20)
+    Force = random.randin(3, 8)
+    Armure = random.randint (0,5)
+    monmonstre = [Nom, PV, Force, Armure]
+    return monmonstre
+
+
+
+
+
+
